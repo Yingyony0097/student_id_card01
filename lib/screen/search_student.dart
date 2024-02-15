c725ef19ab15748e8d6b1744e372e8ee461a0731
saimond <yingyong0097@gmail.com>
2024-02-15 10:51:10 +0700

    dd

--- lib/screen/search_student.dart
+++ lib/screen/search_student.dart
@@ -27,47 +27,47 @@ class _SearchstudentState extends State<Searchstudent> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(244, 255, 254, 254),
       body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 50.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logo.png'),
-                const SizedBox(height: 18),
-                const Text(
-                  'ສະຖາບັນ ເຕັກໂນໂລຊີ ການສື່ສານຂໍ້ມມູນຂ່າວສານ',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              const SizedBox(height: 18),
+              const Text(
+                'ສະຖາບັນ ເຕັກໂນໂລຊີ ການສື່ສານຂໍ້ມມູນຂ່າວສານ',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _searchController,
+                decoration: const InputDecoration(
+                  labelText: 'ປ້ອນລະຫັດນັກສຶກສາ',
+                  labelStyle: TextStyle(color: Colors.green),
+                  border: OutlineInputBorder(),
                 ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: _searchController,
-                  decoration: const InputDecoration(
-                    labelText: 'ປ້ອນລະຫັດນັກສຶກສາ',
-                    labelStyle: TextStyle(color: Colors.green),
-                    border: OutlineInputBorder(),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(192, 39, 2, 226)),
+                  minimumSize: MaterialStateProperty.all(const Size(200, 50)),
+                ),
+                child: const Text(
+                  'ຄົ້ນຫາ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'ຄົ້ນຫາ',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
-}
-
-void main() {
-  runApp(const MaterialApp(
-    home: Searchstudent(),
-  ));
 }

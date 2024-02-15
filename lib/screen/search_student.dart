dce2fbbfbd06c2f6fb1c8cf917237f10bbcf78c7
saimond <yingyong0097@gmail.com>
2024-02-15 11:16:59 +0700

    search_

--- lib/screen/search_student.dart
+++ lib/screen/search_student.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:student_id_card/screen/show_studen_card.dart';
 
 class Searchstudent extends StatefulWidget {
-  const Searchstudent({super.key});
+  const Searchstudent({Key? key});
 
   @override
   State<Searchstudent> createState() => _SearchstudentState();
@@ -28,50 +27,52 @@ class _SearchstudentState extends State<Searchstudent> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(244, 255, 254, 254),
       body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 50.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logo.png'),
-                const SizedBox(height: 18),
-                const Text(
-                  'ສະຖາບັນ ເຕັກໂນໂລຊີ ການສື່ສານຂໍ້ມມູນຂ່າວສານ',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: _searchController,
-                  decoration: const InputDecoration(
-                    labelText: 'ປ້ອນລະຫັດນັກສຶກສາ',
-                    labelStyle: TextStyle(color: Colors.red),
-                    border: OutlineInputBorder(),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              const SizedBox(height: 18),
+              const Text(
+                'ສະຖາບັນ ເຕັກໂນໂລຊີ ການສື່ສານຂໍ້ມມູນຂ່າວສານ',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: const InputDecoration(
+                        labelText: 'ປ້ອນລະຫັດນັກສຶກສາ',
+                        labelStyle: TextStyle(color: Colors.green),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-
-                   onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return const ShowCard();
-                                },
-                              ),
-                            );
-                  },
-                  child: const Text(
-                    'ຄົ້ນຫາ',
-                    style: TextStyle(fontSize: 20),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(192, 39, 2, 226)),
+                      minimumSize:
+                          MaterialStateProperty.all(const Size(100, 50)),
+                    ),
+                    child: const Text(
+                      'ຄົ້ນຫາ',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-
-                 
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
       ),

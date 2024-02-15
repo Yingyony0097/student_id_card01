dc61f89733d9aa4db910fe442ff1d4faf996960f
saimond <yingyong0097@gmail.com>
2024-02-15 10:57:08 +0700

    chage bottom

--- lib/screen/search_student.dart
+++ lib/screen/search_student.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Searchstudent extends StatefulWidget {
-  const Searchstudent({super.key});
+  const Searchstudent({Key? key});
 
   @override
   State<Searchstudent> createState() => _SearchstudentState();
@@ -40,29 +40,37 @@ class _SearchstudentState extends State<Searchstudent> {
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
-              TextField(
-                controller: _searchController,
-                decoration: const InputDecoration(
-                  labelText: 'ປ້ອນລະຫັດນັກສຶກສາ',
-                  labelStyle: TextStyle(color: Colors.green),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(192, 39, 2, 226)),
-                  minimumSize: MaterialStateProperty.all(const Size(200, 50)),
-                ),
-                child: const Text(
-                  'ຄົ້ນຫາ',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: const InputDecoration(
+                        labelText: 'ປ້ອນລະຫັດນັກສຶກສາ',
+                        labelStyle: TextStyle(color: Colors.green),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(192, 39, 2, 226)),
+                      minimumSize:
+                          MaterialStateProperty.all(const Size(100, 50)),
+                    ),
+                    child: const Text(
+                      'ຄົ້ນຫາ',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
@@ -70,4 +78,10 @@ class _SearchstudentState extends State<Searchstudent> {
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: Searchstudent(),
+  ));
 }

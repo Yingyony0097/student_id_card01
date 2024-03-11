8f943c7ca62fad6130a54bb454c781eb9fe0f336
saimond <yingyong0097@gmail.com>
2024-03-11 09:59:24 +0700

    add route

--- servers/index.js
+++ servers/index.js
@@ -236,9 +236,9 @@ app.delete('/student/:id', [
   }
 });
 
- 
+const port = 8000
 app.listen(8000, async () => {
   // เรียกใช้ connectMySQL ตอน start server
   await connectMySQL()
-  console.log('Server started on port 8000')
+  console.log(`Server started on port ${port} `)
 })
